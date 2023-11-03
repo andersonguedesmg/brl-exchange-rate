f87338db462d1a9e6b2f6591dcf94030426c6750
Anderson Guedes <anderson.guedes@outlook.com.br>
2023-11-03 20:14:06 -0300

    Add Header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-
-
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    NotFoundComponent
+    NotFoundComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent,
   ]
 })
 export class SharedModule { }
